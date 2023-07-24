a6a743f4c75e8ce69b8f243abc360f0c9f0f5db3
elkeis <kiryll.e@gmail.com>
2023-07-25 00:32:40 +0300

    dev: a little of design

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,9 +8,14 @@ import { Component, Inject, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   constructor(
+    private router: Router
   ) {}
 
 
   async ngOnInit() {
+  }
+
+  navigate() {
+    this.router.navigate(['/']);
   }
 }
